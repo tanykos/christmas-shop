6b18826936e22f322bdc47e426f6f10cd0110c2a
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2024-12-20 13:59:42 +0100

    Updates

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import '../scss/style.scss'
-import('./api.js');
-import('./utils.js');
-import('./navigation.js');
-import('./home.js');
-import('./slider.js');
-import('./timer.js');
-import('./giftsPage.js');
-import('./modal.js');
